ffe183aec9a1a5cc103f73fb23850f84c98fb905
Alla Makhotka <89968909+dif-dif@users.noreply.github.com>
2022-09-06 10:27:32 +0300

    Update branch.sh

--- 5.09.22/branch.sh
+++ 5.09.22/branch.sh
@@ -9,15 +9,17 @@ while true ; do
             break
     fi
 
-    if [ $number -lt 0 ]; then
-        echo "Error. A number less than zero"; break
-    fi
+    if [ $number -ge 0 ]; then
 
-    case $number in
-        0) echo -e "No students\n" ;;
-        1) echo -e "1 student\n" ;;
-        [2-4]*) echo -e "$number students\n" ;;
-        *) echo -e "A lot of students\n"
-    esac
+        case $number in
+            0) echo -e "No students\n" ;;
+            1) echo -e "1 student\n" ;;
+            [2-4]*) echo -e "$number students\n" ;;
+            *) echo -e "A lot of students\n"
+        esac
+
+    else
+        echo "Error. A number less than zero"
+    fi
 
 done
